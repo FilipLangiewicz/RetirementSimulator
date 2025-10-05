d52744b882543b410a45aeed9d4b80f7ed53e996
MikolajRowicki <01180738@pw.edu.pl>
2025-10-05 10:52:50 +0200

    chatbot

--- simulator/urls.py
+++ simulator/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import AdvisorChatView, advisor_chat_api
 
 app_name = 'simulator'
 
@@ -8,4 +9,7 @@ urlpatterns = [
     path("home/", views.app_home, name="home"),
     path('dashboard/', views.dashboard, name='dashboard'),
     path("profil/rozmowa/", views.conversation_profile, name="conversation_profile"),
+    path('update-profile/', views.update_profile, name='update_profile'),
+    path("doradca/", AdvisorChatView.as_view(), name="advisor_chat"),
+    path("api/doradca/", advisor_chat_api, name="advisor_chat_api"),
 ]
